4fab3f19824f72c6543b765cdd869246a06687c5
Florian Posdziech <hallo@flowfx.de>
2020-05-10 11:58:39 +0200

    Improve publishing script

--- publish.sh
+++ publish.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-echo "octopod build"
+# REMARKS:
+#
+
+# Octopod (i.e. jekyll) builds a static site in the _site directory.
 octopod build
-
-echo "do stuff"
-
 cd _site
 
-rm feed.xml
-
-rm episodes/*
+# We don't want to publish the feed on iTunes or anywhere else. So I'm opting to
+# not paginate it. This is crudely done by setting the page size in the
+# configuration file to 1000 and to delete any feed pages below.
+echo "CREATE TOKEN-BASED FEEDS"
 rm episodes1.mp3.rss
 
 mkdir feed
@@ -23,6 +24,11 @@ done < "$input"
 rm episodes.mp3.rss
 rm tokens.txt
 
+# The episodes htaccess is ignored by the deploy command and has to be added
+# manually on the server. This is to prevent rsync from deleting the softlink
+# that points from the publicly-accessible episodes directory to where the mp3s
+# are actually located in the file system.
+cp ht.access.allowfromall episodes/.htaccess
 cp ht.access.allowfromall feed/.htaccess
 cp ht.access.allowfromall img/.htaccess
 rm ht.access.allowfromall
