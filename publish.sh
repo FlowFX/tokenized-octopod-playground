0896f9cb2ca17b453f8d5945cbf5749802ac52cb
Florian Posdziech <hallo@flowfx.de>
2020-05-10 12:31:11 +0200

    Improve publishing script

--- publish.sh
+++ publish.sh
@@ -9,7 +9,6 @@ cd _site
 # not paginate it. This is crudely done by setting the page size in the
 # configuration file to 1000 and to delete any feed pages below.
 echo "\n## Creating token-based RSS feeds"
-rm episodes1.mp3.rss
 
 # The tokens.txt has to contain 1 token per line. It should be as simple as:
 #
@@ -27,7 +26,7 @@ do
   cp episodes.mp3.rss "feed/$TOKEN.rss"
 done < "$input"
 
-rm episodes.mp3.rss
+rm episodes*.mp3.rss
 rm tokens.txt
 
 # The episodes htaccess is ignored by the deploy command and has to be added
@@ -52,5 +51,5 @@ rm ht.access.allowfromall
 
 cd ..
 
-# Deploy website via Rsync
+# Deploy website via Rsync while excluding from `rsync-exclude`
 octopod deploy
